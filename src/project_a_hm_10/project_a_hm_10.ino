c83bb59c6fd8b41b63f3fd865bc0fbf82d186673
thuc <ledongthuc@gmail.com>
2015-05-29 04:25:49 +0700

    Update: add method set service id

--- src/project_a_hm_10/project_a_hm_10.ino
+++ src/project_a_hm_10/project_a_hm_10.ino
@@ -1,70 +1,19 @@
-#include <SoftwareSerial.h>
+#include <Hm10.h>
+#include <Utility.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
-const int PIN_LIGHT = 3;
-const char TURN_ON_SIGNAL = '1';
-const char TURN_OFF_SIGNAL = '0';
-
-char signalHistory[2];
-
-void setup()  
-{
-  pinMode(PIN_LIGHT, OUTPUT);
+Hm10* hm10;
   
-  Serial.begin(4800);
-  mySerial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  hm10 = new Hm10(10, 11);
 }
 
-void loop() // run over and over
-{
-  if(!mySerial.available()) {
-    return;
-  }
+void loop() {
+  Serial.print("TestCommand, Receive: ");
+  char* result = hm10->setServiceId("0x9999");
+  Serial.print(result);
+  Serial.print("\n\n");
   
-  byte rawData = mySerial.read();
-  Serial.print("Get message: ");
-  Serial.println(rawData, DEC);
-  
-  char* signalHistory = pushToSignalHistory(rawData);
-  char parsedData = composeCharFrom2Bytes(signalHistory[0], signalHistory[1]);
-  
-  switch(parsedData) {
-    case TURN_ON_SIGNAL:
-      turnOnLight();
-      break;
-    case TURN_OFF_SIGNAL:
-      turnOffLight();
-      break;
-  }
+  delay(2000);
 }
 
-void turnOnLight() {
-  Serial.println("Turnon light");  
-  digitalWrite(PIN_LIGHT, HIGH);
-}
-
-void turnOffLight() {
-  Serial.println("Turnon off");  
-  digitalWrite(PIN_LIGHT, LOW);
-}
-
-/*
- * Input 2 bytes, convert them to 1 byte.
- * byte1: 52  |=>  ASCII: 4   |=>  ASCII: 48    |=>    byte: 48  |=>  ASCII: 0  
- * byte2: 56  |        ASCII: 8   |                             |                          |
- *
- *
- * byte1: 52  |=>  ASCII: 4   |=>  ASCII: 49    |=>    byte: 49  |=>  ASCII: 1  
- * byte2: 57  |        ASCII: 9   |                             |                          |
- */
-char composeCharFrom2Bytes(byte first, byte two) {
-  char chars[] = { (char)first, (char)two, '\0'};
-  byte resultAsByte = atoi(chars);
-  return (char)resultAsByte;
-}
-
-char* pushToSignalHistory(byte pushedData) {
-  signalHistory[0] = signalHistory[1];
-  signalHistory[1] = pushedData;
-  return signalHistory;
-}
