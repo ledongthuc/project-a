4fa67c5a2f4c9296cabecb8d50ffc15ccf7534ea
thuc <ledongthuc@gmail.com>
2015-05-29 04:32:58 +0700

    Update: use correct name, moving set service id to setup function

--- src/project_a_hm_10/project_a_hm_10.ino
+++ src/project_a_hm_10/project_a_hm_10.ino
@@ -6,13 +6,15 @@ Hm10* hm10;
 void setup() {
   Serial.begin(9600);
   hm10 = new Hm10(10, 11);
-}
-
-void loop() {
+  
   Serial.print("TestCommand, Receive: ");
   char* result = hm10->setServiceId("0x9999");
   Serial.print(result);
   Serial.print("\n\n");
+}
+
+void loop() {
+
   
   delay(2000);
 }
